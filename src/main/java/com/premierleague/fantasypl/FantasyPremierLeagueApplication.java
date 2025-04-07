16d6fcc773558826c9807279df2a74cc6a85e9f3
DiegoMolinaaa <diego.molina0801@gmail.com>
2025-04-07 10:36:01 -0600

    Spring Project Creation

---
+++ src/main/java/com/premierleague/fantasypl/FantasyPremierLeagueApplication.java
@@ -0,0 +1,13 @@
+package com.premierleague.fantasypl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyPremierLeagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyPremierLeagueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/premierleague/fantasypl/FantasyPremierLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premierleague.fantasypl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyPremierLeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
